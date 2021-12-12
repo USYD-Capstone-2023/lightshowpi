5e6c96003db247b948215b14a1f4b89ed743ea7b
brettreinhard <brett@reinhards.us>
2021-12-11 21:11:36 -0800

    tweet-update: updated christmas tree emoji to match the emoji patches dict value

--- py/tweet.py
+++ py/tweet.py
@@ -37,4 +37,4 @@ ACCESS_SECRET = 'USER INPUTS DATA'
 currenttime = time.strftime("%I:%M:%S")
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
 strin = sys.argv[1]
-api.update_status(status=emoji.emojize(':christmas_tree:') + emoji.emojize(":gift:",use_aliases=True) + strin + emoji.emojize(':christmas_tree:') +  emoji.emojize(':bell:') + " Updated: " + currenttime + emoji.emojize(':bell:'))
+api.update_status(status=emoji.emojize(':Christmas_tree:') + emoji.emojize(":gift:",use_aliases=True) + strin + emoji.emojize(':Christmas_tree:') +  emoji.emojize(':bell:') + " Updated: " + currenttime + emoji.emojize(':bell:'))
